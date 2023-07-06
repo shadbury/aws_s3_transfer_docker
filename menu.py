ec86ec01f4e70c26364c3271f5c06667ab9c7597
Shadbury <joel.hutson@cmd.com.au>
2023-07-07 01:07:39 +0930

    dev environment

--- menu.py
+++ menu.py
@@ -1,16 +1,36 @@
 import tkinter as tk
+from tkinter import messagebox
 
-top = tk.Tk()
+class Window:
+    def __init__(self, master):
+
+        mainframe = tk.Frame(master)
+        mainframe.pack()
+
+        mainmenu = tk.Menu(mainframe)
+        master.config(menu = mainmenu)
+
+        filemenu = tk.Menu(mainframe)
+        filemenu.add_command(label="New File", command = self.func)
+
+        exportmenu = tk.Menu(mainframe)
+        exportmenu.add_command(label="New export", command = self.func)
+        
+        filemenu.add_cascade(label = "export Menu", menu = exportmenu)
+        mainmenu.add_cascade(label = "File Menu", menu = filemenu)
+
+        W = tk.Message(master, text="Hello World")  
+        W.pack()
 
 
-B = tk.Button(top, text="test" )
 
-B.pack()
-
-
-
-top.mainloop()
+    def func(self):
+        print("placeholder")
+    
 
 
-fds
 
+root = tk.Tk()
+root.geometry('300x200')
+window = Window(root)
+root.mainloop()
